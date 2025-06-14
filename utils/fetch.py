f3733fd4a48db0e9c2d54d03b12c349b0b25f6e4
GautamA97 <gautam.annamalai@gmail.com>
2025-06-14 16:25:58 -0400

    data fetch file

--- utils/fetch.py
+++ utils/fetch.py
@@ -1,12 +1,12 @@
 import requests
 import pandas as pd
 
-df = pd.read_csv('movie.metadata.tsv', sep='\t', header=None)
+df = pd.read_csv('data/movie.metadata.tsv', sep='\t', header=None)
 df = df[[0, 2]]
 df.columns = ['wiki_id', 'movie_name']
 print(df.head())
 
-def get_plot_summary(wiki_id_to_find, filepath='plot_summaries.txt'):
+def get_plot_summary(wiki_id_to_find, filepath='data/plot_summaries.txt'):
     with open(filepath, 'r', encoding='utf-8') as f:
         for line in f:
             wiki_id, plot = line.strip().split('\t', 1) 
